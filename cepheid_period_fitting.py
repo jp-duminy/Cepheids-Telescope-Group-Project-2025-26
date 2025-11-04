6ec228a9dd78676319139145d81b0bf73b1a92d5
JP <s2317434@ed.ac.uk>
2025-11-04 17:42:12 +0000

    Added code graveyard; updated period fitting to remove the Lomb-Scargle method; added general astro functions for more flexibility within functions.

--- cepheid_period_fitting.py
+++ cepheid_period_fitting.py
@@ -9,8 +9,7 @@ import scipy
 import emcee as mc
 from matplotlib import pyplot as plt
 import astropy
-from cepheid_analysis import Cepheid_Chi_Error_Analysis
-
+from cepheid_analysis.py import Cepheid_Chi_Error_Analysis
 
 class Cepheid_Period_Finder:
     
@@ -28,13 +27,12 @@ class Cepheid_Period_Finder:
         Converts ISO times to MJD for ease of use.
         """
         t = astropy.time.Time(self.time, scale='utc', format='iso')
-        self.julian_date = t.mjd # modified julian date, simply for the plot
+        self.julian_date = t.mjd # astronomers use modified julian date
         
     def light_curve(self):
         """
         Plot the light curve of each cepheid variable.
         """
-        
         ncols = 3
         nrows = (len(self.time) + ncols - 1) // ncols
         
@@ -53,46 +51,10 @@ class Cepheid_Period_Finder:
             
         ax.set_title('Light Curve for Cepheids')
         plt.show()
+
+    
         
-        
-    # unfinished code
-    def lomb_scargle_2(self, julian_date, magnitude):
-        """
-        Employ Lomb-Scargle periodogram to locate period and then optimise this value.
-        Needs error update.
-        """
-        frequency, power = astropy.timeseries.LombScargle(julian_date, magnitude).autopower()
-        best_freq = frequency[np.argmax(power)]
-        
-        
-    def lomb_scargle_period(self):
-        """
-        Employ Lomb-Scargle periodogram to locate period and then optimise this value. 
-        Need update for errors.
-        """
-        ncols = 3
-        nrows = (len(self.time) + ncols - 1) // ncols
-        fig, ax = plt.subplots(nrows, ncols)
-        
-        self.best_frequencies = []
-        
-        for i in range(len(self.time)):
-            frequency, power = astropy.timeseries.LombScargle(self.julian_date[i], self.magnitude[i]).autopower()
-            best_freq = frequency[np.argmax(power)]
-            self.best_frequencies.append(best_freq)
-            
-            ax[i].plot(frequency, power)
-            ax[i].set_xlabel('Frequency')
-            ax[i].set_ylabel('Lomb-Scargle Power')
-            ax[i].set_title(self.name[i])
-            ax[i].invert_yaxis()
-            ax[i].grid(True)
-            
-        for j in range(len(self.time), len(ax)):
-            ax[j].axis('off')
-        
-        ax.set_title('Power Spectrum from Lomb-Scargle Periodogram')
-        plt.show()
+
         
         
         
