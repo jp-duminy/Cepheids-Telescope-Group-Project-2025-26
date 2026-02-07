b023670ca1f5eba5f4a176d576fc2f95ff7feedd
David-Clayton <s2413476@ed.ac.uk>
2026-02-07 19:10:15 +0000

    Turned heatmaps to log scale

--- Cepheid_apertures.py
+++ Cepheid_apertures.py
@@ -6,6 +6,7 @@ import photutils.psf as psf
 import astropy.io.fits as fits
 from astropy.wcs import WCS
 import matplotlib.pyplot as plt
+from matplotlib.colors import LogNorm
 
 class AperturePhotometry: 
 
@@ -53,7 +54,7 @@ class AperturePhotometry:
         masked_data = mask.cutout(self.data)
     
         if plot == True:
-            plt.imshow(masked_data)
+            plt.imshow(masked_data, norm=LogNorm())
             plt.show()
         
         return masked_data
@@ -69,7 +70,7 @@ class AperturePhotometry:
         #Function expects data to be bkgd subtracted
         #Nan/inf values automatically masked
         if plot == True:
-            plt.imshow(data)
+            plt.imshow(data, norm = LogNorm())
             plt.plot(centroid[0], centroid[1], marker = "+", color = "r")
             plt.show()
 
@@ -91,7 +92,7 @@ class AperturePhotometry:
         #Plot apertures
         if plot == True:
             fig, ax = plt.subplots()
-            ax.imshow(data, origin='lower', interpolation='nearest', cmap='viridis') # Display the image
+            ax.imshow(data, origin='lower', interpolation='nearest', cmap='viridis' , norm=LogNorm) # Display the image
             target_aperture.plot(ax=ax, color='red')
             sky_annulus.plot(ax=ax, color = "white")
             plt.title(f"{ceph_name} taken on {date}")
