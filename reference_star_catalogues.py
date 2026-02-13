4d353e0c1e70b494ae65663e7c21279ce5c6c7a5
JP Duminy <jp@duminy.org>
2026-02-13 16:42:25 +0000

    Reference star catalogues.

---
+++ reference_star_catalogues.py
@@ -0,0 +1,11 @@
+reference_catalogue = {
+    "01": {  # ex: MW Cyg
+        "ref1": {"x-coord": "", "y-coord": "", "V_true": ""}, 
+        "ref2": {"x-coord": "", "y-coord": "", "V_true": ""},
+        # 5 reference stars per cepheid
+    },
+    "02": {
+
+    },
+    # other cepheids
+}
