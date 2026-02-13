ab478aae0813f23505cc61ab827f1ab2d5eadd14
David-Clayton <s2413476@ed.ac.uk>
2026-02-13 17:48:49 +0000

    Cepheids 1-4

--- reference_star_catalogues.py
+++ reference_star_catalogues.py
@@ -1,11 +1,35 @@
 reference_catalogue = {
     "01": {  # ex: MW Cyg
-        "ref1": {"x-coord": "", "y-coord": "", "V_true": ""}, 
-        "ref2": {"x-coord": "", "y-coord": "", "V_true": ""},
+        "ref1": {"x-coord": "1822.2847", "y-coord": "1935.9641", "V_true": "10.81"}, 
+        "ref2": {"x-coord": "1595.6322", "y-coord": "818.53086", "V_true": "8.83"},
+        "ref3": {"x-coord": "2641.3142", "y-coord": "3047.2638", "V_true": "9.33"},
+        "ref4": {"x-coord": "3015.019", "y-coord": "1277.7626", "V_true": "10.93"},
+        "ref5": {"x-coord": "849.90332", "y-coord": "2542.1704", "V_true": "11.39"}
         # 5 reference stars per cepheid
     },
-    "02": {
+    "02": { #V520 Cyg
+        "ref1": {"x-coord": "2368.4585", "y-coord": "3515.9423", "V_true": "8.65"},    
+        "ref2": {"x-coord": "743.3179", "y-coord": "2774.4948", "V_true": "11.33"},
+        "ref3": {"x-coord": "1536.4411", "y-coord": "1546.4145", "V_true": "8.86"},
+        "ref4": {"x-coord": "3017.6114", "y-coord": "802.25364", "V_true": "9.87"},
+        "ref5": {"x-coord": "2369.4495", "y-coord": "1898.5762", "V_true": "9.74"}
 
     },
+    "03": { #VX Cyg
+        "ref1": {"x-coord": "1116.8219", "y-coord": "2639.5012", "V_true": "7.71"}, 
+        "ref2": {"x-coord": "1700.7854", "y-coord": "913.63519", "V_true": "9.42"},
+        "ref3": {"x-coord": "2382.1035", "y-coord": "1150.9701", "V_true": "11.27"},
+        "ref4": {"x-coord": "2874.0096", "y-coord": "1949.4381", "V_true": "10.52"},
+        "ref5": {"x-coord": "3082.3718", "y-coord": "3055.3208", "V_true": "10.68"}
+    },
+    "04": { #VY Cyg
+        "ref1": {"x-coord": "3268.6416", "y-coord": "2095.4098", "V_true": "11.45"}, 
+        "ref2": {"x-coord": "2046.5722", "y-coord": "2818.6226", "V_true": "11.21"},
+        "ref3": {"x-coord": "752.91945", "y-coord": "2919.5407", "V_true": "10.97"},
+        "ref4": {"x-coord": "713.28268", "y-coord": "468.46493", "V_true": "10.67"},
+        "ref5": {"x-coord": "2667.9412", "y-coord": "784.05567", "V_true": "11.27"}
+    },
+
+
     # other cepheids
 }
