5f575086ff8c1ba888d8ea6835cf9162e77fca7b
David-Clayton <s2413476@ed.ac.uk>
2026-02-15 16:23:29 +0000

    Filled in with all Cepheids

--- distance_catalogues.py
+++ distance_catalogues.py
@@ -1,5 +1,13 @@
 cepheid_distances = {
-    "01": {"name": "MW Cyg", "distance": ..., },
-    "02": {"name": "V520 Cyg", "distance": ..., },
-    # ... all 11 cepheids
+    "01": {"name": "MW Cyg", "distance": 1466},
+    "02": {"name": "V520 Cyg", "distance":1849 },
+    "03": {"name": "VX Cyg", "distance": 3322},
+    "04": {"name": "VY Cyg", "distance": 1881 },
+    "05": {"name": "CP Cep", "distance": 4604},
+    "06": {"name": "Z Lac", "distance": 1882 },
+    "07": {"name": "V Lac", "distance": 1631 },
+    "08": {"name": "SW Cas", "distance": 2008 },
+    "09": {"name": "TU Cas", "distance": 808 },
+    "10": {"name": "SZ Cas", "distance": 1688},
+    "11": {"name": "DL Cas", "distance": 605 },
 }
